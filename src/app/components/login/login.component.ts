1d020ca8d60f6329fcdfc8f62de2b4ae32e8366b
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-10-10 13:23:19 +0200

    start task

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +10,24 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+
+  registerForm = this.fb.group({
+
+
+    email:['',[Validators.required]],
+    password:['',[Validators.required]],
+
+  })
+
+  constructor(private fb:FormBuilder){}
+
+
+onSubmit(){
+
+  console.log(this.registerForm.valid)
+  console.log(this.registerForm.value)
+}
+
 
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { CustomValidator } from 'src/app/validators/custom-validator';
 
 @Component({
   selector: 'app-register',
@@ -24,46 +25,69 @@ export class RegisterComponent {
   // });
 
 
-  profileForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: [''],
-    address: this.fb.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: ['']
-    }),
-    aliases: this.fb.array([
-      this.fb.control('')
-    ])
-  });
+  // profileForm = this.fb.group({
+  //   firstName: ['',
+  //   //Validators.required,
+  //   [Validators.required,Validators.minLength(3)]
+  // ],
+  //   lastName: ['',
+  //   //Validators.required,
+  //   [Validators.required,Validators.minLength(3),CustomValidator.checkFirstAndLastUppercase()]
+  // ],
+  //   address: this.fb.group({
+  //     street: [''],
+  //     city: [''],
+  //     state: ['',CustomValidator.checkAddressUSA()],
+  //     zip: ['']
+  //   }),
+  //   aliases: this.fb.array([
+  //     this.fb.control('')
+  //   ])
+  // });
 
+
+  registerForm = this.fb.group({
+
+    username:['',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]],
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,CustomValidator.isPasswordValid()]],
+    country:[''],
+    yob:[new Date().getFullYear(),[Validators.required,CustomValidator.checkNotMinor()]],
+    phoneNumber:[''],
+    gender:['']
+  })
 
   constructor(private fb:FormBuilder){}
 
 
+onSubmit(){
 
-  onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.warn(JSON.stringify(this.profileForm.value,null,4));
-  }
+  console.log(this.registerForm.valid)
+  console.log(this.registerForm.value)
+}
 
-  updateProfile() {
-    this.profileForm.patchValue({
-      firstName: 'Nancy',
-      address: {
-        street: '123 Drew Street',
-        city:'New York'
-      }
-    });
-  }
 
-  getAliases() {
-    return this.profileForm.get('aliases') as FormArray;
-  }
+  // onSubmit() {
+  //   // TODO: Use EventEmitter with form value
+  //   console.warn(JSON.stringify(this.profileForm.value,null,4));
+  // }
 
-  addAlias() {
-    this.getAliases().push(this.fb.control(''));
-  }
+  // updateProfile() {
+  //   this.profileForm.patchValue({
+  //     firstName: 'Nancy',
+  //     address: {
+  //       street: '123 Drew Street',
+  //       city:'New York'
+  //     }
+  //   });
+  // }
+
+  // getAliases() {
+  //   return this.profileForm.get('aliases') as FormArray;
+  // }
+
+  // addAlias() {
+  //   this.getAliases().push(this.fb.control(''));
+  // }
 
 }
